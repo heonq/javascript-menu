5433184087c4bb00a247cdb4e2cded213c314990
heonq <gkagjsrb@naver.com>
2023-12-09 16:25:45 +0900

    feat: 기능 구현에 필요한 메시지와 값을 담은 객체 추가

---
+++ constants/Messages.js
@@ -0,0 +1,21 @@
+const PREFIX = '[ERROR]';
+
+const MESSAGES = Object.freeze({
+  intro: '점심 메뉴 추천을 시작합니다.',
+  coachNameQuery: '코치의 이름을 입력해 주세요. (, 로 구분)\n',
+  menusCantEatQuery: '(코치명)(이)가 못 먹는 메뉴를 입력해주세요.\n',
+  resultHeader: '메뉴 추천 결과입니다.',
+  weekdays: '[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]',
+  categoriesMessage: '카테고리',
+  categories: ['일식', '한식', '중식', '아시안', '양식'],
+  outputFormStart: '[ ',
+  outputFormEnd: ' ]',
+  outputDelimiter: ' | ',
+  end: '추천을 완료했습니다.',
+  lineBreak: '',
+  invalidLength: `${PREFIX} 코치 이름은 2글자 이상 4글자 이하로 입력해 주세요.`,
+  duplicated: `${PREFIX} 중복된 이름은 입력할 수 없습니다.`,
+  invalidCoachCount: `${PREFIX} 코치는 2명 이상 5명 이하로 입력해 주세요.`,
+  invalidMenuCount: `${PREFIX} 못 먹는 메뉴는 0개 이상 2개 이하로 입력해 주세요.`,
+  invalidMenu: `${PREFIX} 메뉴목록에 존재하는 메뉴를 입력해 주세요.`,
+});
---
+++ constants/Values.js
@@ -0,0 +1,11 @@
+const VALUES = Object.freeze({
+  minimumNameLength: 2,
+  maximumNameLength: 4,
+  minimumCoachCount: 2,
+  maximumCoachCount: 5,
+  minimumMenuCount: 0,
+  maximumMenuCount: 2,
+  maximumCategoriesRepeat: 2,
+});
+
+export default VALUES;
